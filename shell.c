f3ed60e5eb4d07bd2823d6b471fd3a4f260098df
manolobkno08 <manologm10@hotmail.com>
2021-08-16 01:07:04 -0500

    add test tokenizer prompt strtok_test

---
+++ shell.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int getStringLenght(char *string)
+{
+    int i;
+    for (i = 0; string[i] != '\0'; i++)
+    ;
+
+    return (i);
+}
+
+int main()
+{
+    char *prompt = "./$ ";
+
+    /* define puntero donde voy a almacenar lo que captura getline */
+    char *line;
+    /* entero que va a indicar cuanto va a valer line */
+    size_t lineSize = 0;
+    /* guardar caracteres que capture */
+    int charactersRead = 0;
+
+    while (1)
+    {
+        write(STDOUT_FILENO, prompt, getStringLenght(prompt));
+
+        /* recibe los argumentos: recibe  */
+        charactersRead = getline(&line, &lineSize, stdin);
+
+        write(STDOUT_FILENO, line, charactersRead);
+    }
+
+    return (0);
+}
---
+++ token.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char *_strtok(char * str, const char * delimiter)
+{
+    return (str);
+}
+
+char **tokenizer(char *buffer, char *delimiter)
+{
+    char **tokens = NULL;
+    size_t i = 0;
+    unsigned int mcount = 1;
+
+    if (buffer == NULL)
+    {
+        return (NULL);
+    }
+
+    tokens = malloc(sizeof(char *) * mcount);
+
+    if((*buffer == '\n' || *buffer == ' ') && *(buffer + 1) == '\0')
+    {
+        return (NULL);
+    }
+
+    while ((tokens[i] = strtok(buffer, delimiter)) != NULL)
+    {
+        i++;
+        if (i >= mcount)
+        {
+            tokens = realloc(tokens, sizeof(char *) * (i + 1));
+            if (tokens == NULL)
+                return(NULL);
+        }
+        buffer = NULL;
+        
+    }
+    return (tokens);
+}
+
+void main()
+{
+    char buffer[] = "Hola Mundo Lindo";
+    char *delimiter = " \n";
+    char **array;
+    
+    array = tokenizer(buffer, delimiter);
+
+    while (*array)
+    {
+        printf("%s\n", *array);
+        array++;
+    }
+    
+}
