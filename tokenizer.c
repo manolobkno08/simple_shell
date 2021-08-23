7b318590350dede9324e393b1ecdeef8a006e505
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-23 17:42:05 -0500

    add tokenizer

---
+++ tokenizer.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+char **tokenizer(char *buffer, char *delimiter)
+{
+	char **tokens = NULL;
+	size_t i = 0;
+	unsigned int mcount = 1;
+
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
+
+	tokens = malloc(sizeof(char *) * mcount);
+
+	if((*buffer == '\n' || *buffer == ' ') && *(buffer + 1) == '\0')
+	{
+		return (NULL);
+	}
+
+	while ((tokens[i] = _strtok(buffer, delimiter)) != NULL)
+	{
+		i++;
+		if (i >= mcount)
+		{
+			tokens = realloc(tokens, sizeof(char *) * (i + 1));
+			if (tokens == NULL)
+				return(NULL);
+		}
+		buffer = NULL;
+		
+	}
+	return (tokens);
+}
