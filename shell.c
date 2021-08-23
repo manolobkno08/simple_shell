bc03ad26df99a37a7b1b9e0f710e5f9b335cd24f
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-23 17:43:09 -0500

    add main

--- shell.c
+++ shell.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int main()
+int main( int *argc, char **argv, char ** environment)
 {
 	vars_t vars = {NULL};
 
@@ -10,44 +10,141 @@ int main()
 
 	char (*f)(vars_t * r);
 
+	int acc;
+
 	/* entero que va a indicar cuanto va a valer line */
 	size_t lineSize = 0;
 	/* guardar caracteres que capture */
 	int charactersRead = 0;
 
-	write(STDOUT_FILENO, prompt, getStringLenght(prompt));
+	if(isatty(STDIN_FILENO))
+	{
+		printf("%s", prompt);
+	}
 
+	/* BUCLE SHELL */
 	while (charactersRead = getline(&vars.buffer, &lineSize, stdin) != -1)
 	{
 		vars.array = tokenizer(vars.buffer, delimiter);
 
 		if(vars.array == NULL)
 		{
-			write(STDOUT_FILENO, prompt, getStringLenght(prompt));
+			if(isatty(STDIN_FILENO))
+			{
+				printf("%s", prompt);
+			}
 			continue;
 		}
 		else
 		{
 			f = match(&vars);
-			if(f == NULL)
-			{
-				printf("No such file or directory");
-				putchar('\n');
-				write(STDOUT_FILENO, prompt, getStringLenght(prompt));
-				continue;
-			}
-			else
+
+			if(f != NULL)
 			{
 				f(&vars);
 			}
 
-			write(STDOUT_FILENO, prompt, getStringLenght(prompt));
+			else if(f == NULL)
+			{
+				if( acc = access(vars.array[0], F_OK) != -1)
+				{
+					pid_t child = fork();
+					int status;
+
+					switch(child)
+					{
+						case 0:
+						execve(vars.array[0], vars.array, environment);
+						break;
+						
+						case -1:
+						printf("Error to create a child proccess");
+						break;
+
+						default:
+						wait(&status);
+						break;
+					}
+				}
+				int i;
+				/* buscar path en env */
+				for(i = 0; environment[i]; i++)
+				{
+					int verCompare;
+					if ((verCompare = MyStrNCmp(environment[i], "PATH", 4)) == 0)
+					{
+						break;
+					}
+					
+				}
+				char *duplicate;
+				duplicate = myStrDup(environment[i]);
+
+				char **tokensEnv;
+				tokensEnv = tokenizer(duplicate, ":=");
+
+				i = 0;
+				char * concat1;
+				char * concat2;
+
+				while(tokensEnv[i] != NULL)
+				{
+					concat1 = mystrCat(tokensEnv[i], "/");  /*   /usr/bin/   */
+
+					concat2 = mystrCat(concat1, vars.array[0]);  /*   /usr/bin/ls   */
+
+					if(access(concat2, F_OK) == 0)
+					{
+						pid_t child = fork();
+						int status;
+
+						switch(child)
+						{
+							case 0:
+							execve(concat2, vars.array, environment);
+							break;
+							
+							case -1:
+							printf("Error to create a child proccess");
+							break;
+
+							default:
+							wait(&status);
+							break;
+						}
+						break;
+					}
+					i++;
+				}
+				continue;
+
+				if(isatty(STDIN_FILENO))
+				{
+					printf("No such file or directory");
+					putchar('\n');
+				}
+				
+				if(isatty(STDIN_FILENO))
+				{
+					printf("%s", prompt);
+				}
+				continue;
+			}
+
+			if(isatty(STDIN_FILENO))
+			{
+				printf("%s", prompt);
+			}
 		}
 
 		
 	}
 
-	putchar('\n');
+	if(isatty(STDIN_FILENO))
+	{
+		putchar('\n');
+	}
+	
 
 	return (0);
 }
