59258cd5850da6e22eb42535a89582ec201a12ff
manolobkno08 <manologm10@hotmail.com>
2021-08-25 16:11:12 -0500

    last update main branch

--- env.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main( int argc, char *argv[], char *envp[] )
-{
-    int i;
-    /* echo all environment args */
-    for (i = 0 ; envp[i] ; i++)
-    printf( "envp[%d]: %s\n", i, envp[i] );
-}
